7565a9dd845bcf6e3503153790560cd91f49507e
pierotibou <pierotibou@users.noreply.github.com>
2022-05-25 16:50:20 +0000

    [.Net Version Bump] Bump to v3.4.600

--- dotnet/build-packages.sh
+++ dotnet/build-packages.sh
@@ -1,8 +1,8 @@
 
-RELEASE_VERSION="2.1.200"
-DEVELOPMENT_VERSION="0.1.60-prerelease"
-AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.32.4-1-x86_64.zip"
-TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v2.1.2/windows-tracer-home.zip"
+RELEASE_VERSION="3.4.600"
+DEVELOPMENT_VERSION="0.1.61-prerelease"
+AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.36.0-1-x86_64.zip"
+TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v3.4.6/windows-tracer-home.zip"
 
 echo "Downloading tracer from ${TRACER_DOWNLOAD_URL}"
 wget -O tracer.zip $TRACER_DOWNLOAD_URL
